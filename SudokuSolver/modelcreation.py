6b7c6a2fbcf34e274ea7e783aafcbd497efcfa68
Shubham Bhatia <39002688+drametoid@users.noreply.github.com>
2023-04-09 16:02:02 -0700

    Updates core files * updates modelcreation.py to allow more commandline configs and train or load previous model * updates requirements.txt

--- SudokuSolver/modelcreation.py
+++ SudokuSolver/modelcreation.py
@@ -1,10 +1,11 @@
 import keras
 import numpy as np
 from model import get_model
-from preprocess import load_data
+from preprocess_data import load_data
 from solve_sudoku import inference_sudoku
 
 import sys
+import os
 
 def train_model(x_train, y_train):
     model = get_model()
@@ -26,20 +27,37 @@ def test_accuracy(feats, labels):
             correct += 1
     print(f'Accuracy is {correct/feats.shape[0]}')
 
-def main(input_file, saved_model=None, get_test_accuracy=False):
+def main(input_file, saved_model_path=None, get_test_accuracy=False):
     x_train, x_test, y_train, y_test = load_data(input_file)
-    if saved_model is None:
+    if saved_model_path is (None or '.'):
+        print(f'Training and saving model now...')
         model = train_model(x_train, y_train)
         model.save('model/sudoku.h5')
     else:
-        model = keras.models.load_model(saved_model)
-
-    if(get_test_accuracy):
-        test_accuracy(x_test[:100], y_test[:100])
+        if os.path.isfile(saved_model_path):
+            try:
+                print(f'Found saved model. Loading now..')
+                model = keras.models.load_model(saved_model_path)
+                print(f'Model loaded!')
+            except Exception as e:
+                print(f'Error encountered: {e}')
+                sys.exit(1)
+        else:
+            print('ERROR: Unable to load model or wrong path provided!')
+            sys.exit(1)
     
 
 if __name__ == '__main__':
+    print(f'''Following input arguments need to be provided:\n
+    1. csv file for model training
+    2. Model file path (if exists). If re-training or doesn't exist give '.' as input (without the ')
+    3. getAccuracyData flag: 1 or 0 as boolean input (100 puzzles are tested to get accuracy of the model)
+    '''
+        )
     input_file = sys.argv[1]
-    model = sys.argv[2]
+    model_path = sys.argv[2]
     get_test_accuracy = bool(sys.argv[3])
-    main(input_file, model, get_test_accuracy)
+    print(f'Input file provided: {input_file}')
+    print(f'Model file provided: {model_path}')
+    print(f'Accuracy requested: {get_test_accuracy}')
+    main(input_file, model_path, get_test_accuracy)
